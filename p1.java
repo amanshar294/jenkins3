d4ba2fff9661e40637fe281ae434ad7af68816f6
amanshar294 <97936407+amanshar294@users.noreply.github.com>
2022-01-28 08:40:03 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,14 @@
+class P14 
+{
+	public static void add(int a,int b) 
+	{
+		int c = a+b;
+		System.out.println("c");
+	}
+		public static void main(String[] args) 
+	{
+		System.out.println("MB");
+		add(10,20);
+		System.out.println("ME");
+	}
+}
